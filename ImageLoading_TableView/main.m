a58d514691264af378a50a8b02c430bc5d1302c5
Urvashi-iPhone <lathiyaurvashi36@gmail.com>
2016-12-30 09:45:42 +0530

    Initial Commit

---
+++ ImageLoading_TableView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ImageLoading_TableView
+//
+//  Created by Tecksky Techonologies on 12/30/16.
+//  Copyright © 2016 Tecksky Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
